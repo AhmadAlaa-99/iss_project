6e77f4dd82d0a05dabcb92fe9718025638d48f94
AhmadAlaa-99 <Ahmadite99@gmail.com>
2023-12-28 20:37:35 +0300

    FixExceptionsConnection

--- Client/controller/ClientController.py
+++ Client/controller/ClientController.py
@@ -8,11 +8,20 @@ class Client:
         self.port = port
 
     async def handle(self):
-        self.re_sock, self.wr_sock = await asyncio.open_connection(self.address, self.port, family=sk.AF_INET)
+        try:
+            self.re_sock, self.wr_sock = await asyncio.open_connection(self.address, self.port, family=sk.AF_INET)
+            while not self.wr_sock.is_closing():
+                self.wr_sock.write('Hello, Server From Client'.encode(encoding='utf8'))
+                await self.wr_sock.drain()
 
-        self.wr_sock.write('Hello, Server From Client'.encode(encoding='utf8'))
-        await self.wr_sock.drain()
-
-        data = await self.re_sock.read(100)
-        print(data.decode(encoding='utf8'))
-        self.wr_sock.close()
+                data = await self.re_sock.read(1024)
+                msg = data.decode(encoding='utf8')
+                if not self.re_sock.at_eof():
+                    print(msg)
+                    print(len(msg))
+                else:
+                    self.wr_sock.close()
+                # self.wr_sock.close()
+                # await self.wr_sock.wait_closed()
+        except ConnectionRefusedError:
+            print("No Server Respond")
